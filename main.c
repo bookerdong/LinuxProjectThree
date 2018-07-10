17cdde492508b11e386628a8ad190c1120ac6d98
bookerdong <946613746@qq.com>
2018-07-10 18:16:52 +0800

    1

--- main.c
+++ main.c
@@ -1,7 +1,100 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <termios.h>
+#include <string.h>
+
+int set_opt(int fd,int nSpeed, int nBits, char nEvent, int nStop)
+{
+/* 五个参量 fd打开文件 speed设置波特率 bit数据位设置   neent奇偶校验位 stop停止位 */
+    struct termios newtio,oldtio;
+    if ( tcgetattr( fd,&oldtio) != 0) {
+        perror("SetupSerial 1");
+        return -1;
+    }
+    bzero( &newtio, sizeof( newtio ) );
+    newtio.c_cflag |= CLOCAL | CREAD;
+    newtio.c_cflag &= ~CSIZE;
+    switch( nBits )
+    {
+    case 7:
+        newtio.c_cflag |= CS7;
+    break;
+    case 8:
+        newtio.c_cflag |= CS8;
+    break;
+    }
+    switch( nEvent )
+    {
+    case 'O':
+        newtio.c_cflag |= PARENB;
+        newtio.c_cflag |= PARODD;
+        newtio.c_iflag |= (INPCK | ISTRIP);
+        break;
+    case 'E':
+        newtio.c_iflag |= (INPCK | ISTRIP);
+        newtio.c_cflag |= PARENB;
+        newtio.c_cflag &= ~PARODD;
+        break;
+    case 'N':
+        newtio.c_cflag &= ~PARENB;
+        break;
+    }
+switch( nSpeed )
+    {
+    case 2400:
+        cfsetispeed(&newtio, B2400);
+        cfsetospeed(&newtio, B2400);
+        break;
+    case 4800:
+        cfsetispeed(&newtio, B4800);
+         cfsetospeed(&newtio, B4800);
+        break;
+    case 9600:
+        cfsetispeed(&newtio, B9600);
+        cfsetospeed(&newtio, B9600);
+        break;
+    case 115200:
+        cfsetispeed(&newtio, B115200);
+        cfsetospeed(&newtio, B115200);
+        break;
+    default:
+        cfsetispeed(&newtio, B9600);
+        cfsetospeed(&newtio, B9600);
+        break;
+    }
+    if( nStop == 1 )
+        newtio.c_cflag &= ~CSTOPB;
+    else if ( nStop == 2 )
+    newtio.c_cflag |= CSTOPB;
+    newtio.c_cc[VTIME] = 0;
+    newtio.c_cc[VMIN] = 0;
+    tcflush(fd,TCIFLUSH);
+    if((tcsetattr(fd,TCSANOW,&newtio))!=0)
+    {
+        perror("com set error");
+        return -1;
+    }
+    printf("set done!\n");
+    return 0;
+}
 
 int main(void)
 {
-	printf("Hello Serail\n");
+	////test github
+	// printf("Hello Serail\n");
+	
+	int fd = -1;
+	fd = open("/dev/ttyUSB0",O_RDWR|O_NOCTTY|O_NDELAY);
+	if(fd < 0)
+	{
+		perror("open");
+	}
+	
+	int i = set_opt(fd,921600,8,'N',1);
+	
+	close(fd);
 	return 0;
 }
